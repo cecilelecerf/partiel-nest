86817268bc610ea1bd807d00de6be3bb9b0f4bf2
cecilelecerf <c.lecerf.cecile@gmail.com>
2026-03-31 16:03:45 +0200

    fix name

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
-import { TrainingsModule } from './training/trainings.module';
+import { TrainingsModule } from './trainings/trainings.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
